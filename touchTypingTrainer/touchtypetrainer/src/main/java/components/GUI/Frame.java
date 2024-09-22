7c4274b8f83a096eaafec84eb4f371f168eb58f5
bfararjeh <bahafararjeh@hotmail.com>
2024-09-22 18:29:59 +0100

    working on installing font
    tidying up, experimenting with gui aesthetics, trying to install a fucking font but java is confusing

--- touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/Frame.java
+++ touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/Frame.java
@@ -2,46 +2,54 @@ package components.GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.InputStream;
 
 /*
  * class that generates the frame to be used
  */
+@SuppressWarnings("unused")
 public class Frame extends JFrame {
 
-    private JButton button1;
-    private JButton button2;
-    private JPanel panel;
-    private JTextArea textArea;
-
+    private JLabel labelOne;
+    private JPanel mainPanel;
 
     public Frame(){
         super("TouchTyper");
 
+        // calls "setUiFont"
+        setUIFont(new javax.swing.plaf.FontUIResource(
+            "MONOSPACED",0, 50));
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(1000, 750));
-        setTitle("Touch Type Trainer");  
+        setTitle("Touch Typing Trainer");  
         setResizable(false);
         setVisible(true);
+
         
+        labelOne = new JLabel();
+        labelOne.setText("A String");
+        labelOne.setLayout(new BorderLayout());
         
-        setLayout(new BorderLayout());
-        panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBackground(Color.CYAN);
+        mainPanel.add(labelOne);
 
-        button1 = new JButton("Click me");
-        button1.setMaximumSize(new Dimension(400, 400));
-
-        button2 = new JButton("This is a long piece of text");
-
-        textArea = new JTextArea("i love christmas");
-        textArea.setMaximumSize(new Dimension(1750, 200));
-
-        add(panel, BorderLayout.CENTER);
-
-        panel.add(button1, Component.CENTER_ALIGNMENT);
-        panel.add(button2);
-        panel.add(textArea);
-
+        add(mainPanel);
         pack();
     }
+
+    // function that ascertains all font is of correct "default" font
+    @SuppressWarnings("rawtypes")
+    public static void setUIFont (javax.swing.plaf.FontUIResource f){
+        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get (key);
+            if (value instanceof javax.swing.plaf.FontUIResource)
+            UIManager.put (key, f);
+            }
+        } 
 }
+
--- touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/GUIMain.java
+++ touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/GUIMain.java
@@ -3,7 +3,12 @@ package components.GUI;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 /*
  * class that generates a GUI for project
@@ -11,10 +16,14 @@ import java.awt.Image;
 public final class GUIMain {
 
     private Frame frame;
+    private static InputStream myStream = null;
+    private static final String FONT_PATH_VERAMONO = "src/main/resources/VeraMono.ttf";
     
     public GUIMain(){
 
         SwingUtilities.invokeLater(new Runnable(){
+            // functino that builds the frame. it also:
+            //  locates the icon image
             public void run() {
                 frame = new Frame();
 
@@ -23,12 +32,28 @@ public final class GUIMain {
                     icon = ImageIO.read(ClassLoader.getSystemResource(
                         "icon.png"));
                     frame.setIconImage(icon);
+                
                 } catch (Exception e) {
                     System.out.println("Caught an Exception!" 
-                    + "'icon.png' cannot be found");
+                    + " 'icon.png' cannot be found");
                     e.printStackTrace();
-                }
-            }
-        });
+                    }
+
+                    
+                try {
+
+                    myStream = new BufferedInputStream(new FileInputStream(FONT_PATH_VERAMONO));
+
+                    GraphicsEnvironment ge = 
+                        GraphicsEnvironment.getLocalGraphicsEnvironment();
+                    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, myStream));
+                    
+                } catch (Exception e) {
+                    System.out.println("Caught an Exception!" 
+                    + " Error loading 'VeraMono.ttf'");
+                    e.printStackTrace();
+                    }
+                 }       
+            });
+        }
     }
-}
