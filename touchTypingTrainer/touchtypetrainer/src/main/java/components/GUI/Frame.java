d00a28b1848e23683012eb2292dcbde79c0af65a
bfararjeh <bahafararjeh@hotmail.com>
2024-09-23 19:54:55 +0100

    continuing work
    - installed font (finally)
    - function to set installed font as default implemented
    - created new component "ColorScheme"
    - experimenting with "GridBagLayout"

--- touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/Frame.java
+++ touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/Frame.java
@@ -7,34 +7,52 @@ import java.io.InputStream;
 /*
  * class that generates the frame to be used
  */
-@SuppressWarnings("unused")
 public class Frame extends JFrame {
 
     private JLabel labelOne;
     private JPanel mainPanel;
+    private Font VeraMono;
 
     public Frame(){
         super("TouchTyper");
 
-        // calls "setUiFont"
-        setUIFont(new javax.swing.plaf.FontUIResource(
-            "MONOSPACED",0, 50));
+        // try/catch statement that pulls 'VeraMono.ttf'
+        try {
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream iStream = classloader.getResourceAsStream("VeraMono.ttf");
 
+            Font rawFont = Font.createFont(Font.TRUETYPE_FONT, iStream);
+            VeraMono = rawFont.deriveFont(0, 50);
+
+            GraphicsEnvironment ge = 
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(VeraMono);
+            
+        } catch (Exception e) {
+            System.out.println("Caught an Exception!" 
+            + " Error loading 'VeraMono.ttf'");
+            e.printStackTrace();
+            }
+
+
+        setUIFont(new javax.swing.plaf.FontUIResource(VeraMono));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(1000, 750));
         setTitle("Touch Typing Trainer");  
         setResizable(false);
         setVisible(true);
 
+
         
         labelOne = new JLabel();
         labelOne.setText("A String");
         labelOne.setLayout(new BorderLayout());
+        labelOne.setForeground(ColorScheme.text1);
         
         mainPanel = new JPanel();
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.setBackground(Color.CYAN);
+        mainPanel.setLayout(new GridBagLayout());
         mainPanel.add(labelOne);
+        mainPanel.setBackground(ColorScheme.background1);
 
         add(mainPanel);
         pack();
--- touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/GUIMain.java
+++ touchTypingTrainer/touchtypetrainer/src/main/java/components/GUI/GUIMain.java
@@ -3,12 +3,7 @@ package components.GUI;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import java.awt.Font;
-import java.awt.GraphicsEnvironment;
 import java.awt.Image;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.InputStream;
 
 /*
  * class that generates a GUI for project
@@ -16,8 +11,6 @@ import java.io.InputStream;
 public final class GUIMain {
 
     private Frame frame;
-    private static InputStream myStream = null;
-    private static final String FONT_PATH_VERAMONO = "src/main/resources/VeraMono.ttf";
     
     public GUIMain(){
 
@@ -36,21 +29,6 @@ public final class GUIMain {
                 } catch (Exception e) {
                     System.out.println("Caught an Exception!" 
                     + " 'icon.png' cannot be found");
-                    e.printStackTrace();
-                    }
-
-                    
-                try {
-
-                    myStream = new BufferedInputStream(new FileInputStream(FONT_PATH_VERAMONO));
-
-                    GraphicsEnvironment ge = 
-                        GraphicsEnvironment.getLocalGraphicsEnvironment();
-                    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, myStream));
-                    
-                } catch (Exception e) {
-                    System.out.println("Caught an Exception!" 
-                    + " Error loading 'VeraMono.ttf'");
                     e.printStackTrace();
                     }
                  }       
